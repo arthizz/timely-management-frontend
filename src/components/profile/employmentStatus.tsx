61cd7d0b28e120665c87de17dd8b6a79489bedc9
John Arthur <johnarthurbadiango3011@gmail.com>
2026-02-24 16:27:08 +0800

    In progress Employment Status Card

--- src/components/profile/employmentStatus.tsx
+++ src/components/profile/employmentStatus.tsx
@@ -5,7 +5,14 @@ export default function EmploymentStatus(){
     return (
 
         <div className="shadow-lg px-5 py-10 rounded-xl bg-white mb-10">
-            <h2 className="text-blue-900 font-semibold text-lg">Employment Status: </h2>
+            <div className="flex justify-between">
+                <div>
+                    <h2 className="text-blue-900 font-semibold text-lg">Employment Status: </h2>
+                </div>
+                <div className="px-3 py-1 bg-green-500/70 text-white rounded-3xl">
+                    <p>Employed</p>
+                </div>
+            </div>
         </div>
 
     );
