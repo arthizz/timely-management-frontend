01ec20bf9f2a2a569e6257cbe229e359a2e0164a
John Arthur <johnarthurbadiango3011@gmail.com>
2026-02-27 17:03:06 +0800

    Added leave form request in progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 
 import "./globals.css";
+import "./global.css";
 
 const poppinsSans = Poppins({
   variable: "--font-poppins-sans",
