e61d50057eb4e3524c107c97da88edd165f82c69
John Arthur Badiango <johnarthurbadiango3011@gmail.com>
2026-03-11 20:21:14 +0800

    added dynamic selected event in leave type

--- src/components/leaveform/leavePurpose.tsx
+++ src/components/leaveform/leavePurpose.tsx
@@ -8,10 +8,13 @@ import PaternitySvg from "../svgIcons/paternitySvg";
 import { ChangeEvent, useState } from "react";
 export default function LeavePurpose(){
 
-    const [togglePersonal, setTogglePersonal] = useState(false);
+    const [togglePersonal, setTogglePersonal] = useState<boolean>(true);
+    const [selectedLeave, setSelectedLeave] = useState<string>("personal");
     let personalLeaveClasses = togglePersonal ? "w-full mb-3 text-gray-500" : "hidden";
 
     const togglePersonalLeave = (event: ChangeEvent<HTMLInputElement>) => {
+
+        setSelectedLeave(event.target.value);
 
         if(event.target.value == "personal"){
 
@@ -32,7 +35,7 @@ export default function LeavePurpose(){
         <>
             <div className="text-gray-500 grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
                 <div className="radio">
-                    <input type="radio" id="sick-leave" name="leave-purpose" value="sick-leave" onChange={togglePersonalLeave} />
+                    <input type="radio" id="sick-leave" name="leave-purpose" value="sick-leave" onChange={togglePersonalLeave} checked={selectedLeave == "sick-leave"}/>
                     <label htmlFor="sick-leave" className="border-solid border-2 border-grey bg-white block shadow-md rounded-lg text-sm">
                         <div className="flex justify-between items-center px-4 py-2">
                             <div className="uppercase tracking-wide"><strong>Sick Leave</strong></div>
@@ -41,7 +44,7 @@ export default function LeavePurpose(){
                     </label>
                 </div>
                 <div className="radio">
-                    <input type="radio" id="bereavement" name="leave-purpose" value="bereavement" onChange={togglePersonalLeave}/>
+                    <input type="radio" id="bereavement" name="leave-purpose" value="bereavement" onChange={togglePersonalLeave} checked={selectedLeave == "bereavement"}/>
                     <label htmlFor="bereavement" className="border-solid border-2 border-grey bg-white block shadow-md rounded-lg">
                         <div className="flex justify-between items-center px-4 py-2 text-sm">
                             <div className="uppercase tracking-wide"><strong>Bereavement</strong></div>
@@ -50,7 +53,7 @@ export default function LeavePurpose(){
                     </label>
                 </div>
                 <div className="radio">
-                    <input type="radio" id="maternity" name="leave-purpose" value="maternity" onChange={togglePersonalLeave} />
+                    <input type="radio" id="maternity" name="leave-purpose" value="maternity" onChange={togglePersonalLeave} checked={selectedLeave == "maternity"}/>
                     <label htmlFor="maternity" className="border-solid border-2 border-grey bg-white block shadow-md rounded-lg">
                         <div className="flex justify-between items-center px-4 py-2 text-sm">
                             <div className="uppercase tracking-wide"><strong>Maternity</strong></div>
@@ -59,7 +62,7 @@ export default function LeavePurpose(){
                     </label>
                 </div>
                 <div className="radio">
-                    <input type="radio" id="paternity" name="leave-purpose" value="paternity" onChange={togglePersonalLeave}/>
+                    <input type="radio" id="paternity" name="leave-purpose" value="paternity" onChange={togglePersonalLeave} checked={selectedLeave == "paternity"}/>
                     <label htmlFor="paternity" className="border-solid border-2 border-grey bg-white block shadow-md rounded-lg text-sm">
                         <div className="flex justify-between items-center px-4 py-2">
                             <div className="uppercase tracking-wide"><strong>Paternity</strong></div>
@@ -68,7 +71,7 @@ export default function LeavePurpose(){
                     </label>
                 </div>
                 <div className="radio">
-                    <input type="radio" id="personal" name="leave-purpose" value="personal" onChange={togglePersonalLeave}/>
+                    <input type="radio" id="personal" name="leave-purpose" value="personal" onChange={togglePersonalLeave} checked={selectedLeave == "personal"}/>
                     <label htmlFor="personal" className="border-solid border-2 border-grey bg-white block shadow-md rounded-lg text-sm">
                         <div className="flex justify-between items-center px-4 py-2">
                             <div className="uppercase tracking-wide"><strong>Personal</strong></div>
